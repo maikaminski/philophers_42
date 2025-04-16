992300e021753a1f5b11ef95007bf652480409f1
Maira Kaminski <mahkaminski@gmail.com>
2025-04-16 17:22:42 -0300

    organizing files

---
+++ philo/philo_monito.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_monito.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makamins <makamins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 17:14:25 by makamins          #+#    #+#             */
+/*   Updated: 2025/04/16 17:17:35 by makamins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+monitor_deaths(&data);
+
+has_died(philo);
+
+all_full(&data); //só se for passado o número minimo de refeições para cada
