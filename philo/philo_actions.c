1a3f3f109bf83281814ba48e28045bb1a8d3e9ad
Maira Kaminski <mahkaminski@gmail.com>
2025-05-15 19:37:14 -0300

    .

--- philo/philo_actions.c
+++ philo/philo_actions.c
@@ -45,10 +45,7 @@ void	philo_sleep(t_philo *philo)
 	uint64_t	start;
 
 	if (should_stop(philo))
-	{
-		release_forks(philo);
 		return ;
-	}
 	start = get_time();
 	safe_print(philo, SLEEP);
 	usleep(philo->data->time_to_sleep * 1000);
@@ -60,10 +57,7 @@ void	philo_think(t_philo *philo)
 	int			time_to_think;
 
 	if (should_stop(philo))
-	{
-		release_forks(philo);
 		return ;
-	}
 	start = get_time();
 	safe_print(philo, THINK);
 	if (philo->data->time_to_think > 10)
